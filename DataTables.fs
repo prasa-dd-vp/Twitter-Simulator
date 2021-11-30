74010b8552eb9b483c812f831f02d5fb56544797
prasa_dd_vp <prasad.venkatesh90@gmail.com>
2021-11-30 14:10:38 -0500

    Deleted fs files

--- DataTables.fs
+++
@@ -1,8 +0,0 @@
-module DataTables
-open System.Data
-
-let dataSet = new DataSet()
-
-//Tables
-let userDT = new DataTable("User_Data_Table")
-userDT.Columns.Add("")
--- Utils.fs
+++
@@ -1,9 +0,0 @@
-module Utils
-
-open System
-open System.Data
-open DataTables
-
-let registerUser (userId:string) = 
-    userDT.Rows.Add(userid) |> ignore
-    usersTable.AcceptChanges()
